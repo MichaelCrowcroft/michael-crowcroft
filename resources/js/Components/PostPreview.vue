a04913a78ea37b2d2864b0383ad8a187612ffa5c
Michael Crowcroft <michaelcrowcroft@outlook.com>
2023-11-11 21:55:17 -0500

    .

--- resources/js/Components/PostPreview.vue
+++ resources/js/Components/PostPreview.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
     <Link :href="'posts/' + post.slug">
-        <div class="bg-pink-100 backdrop-blur shadow bg-opacity-25 py-4 px-8 mb-8 transition ease-in-out duration-50 hover:cursor-pointer hover:shadow-lg">
+        <div class="bg-pink-100 backdrop-blur shadow bg-opacity-25 p-4 mb-8 transition ease-in-out duration-50 hover:cursor-pointer hover:shadow-lg lg:p-8">
             <p class="uppercase text-xs font-bold text-cyan-600">{{ post.category }}</p>
             <div class="flex justify-between mb-4 items-center">
                 <h3 class="text-md leading-none font-black text-slate-900">
@@ -21,7 +21,7 @@ defineProps({
                     <DateFormatter :date="post.published_at" />
                 </div>
             </div>
-            <p class="text-xs leading-relaxed mb-4">
+            <p class="text-xs leading-relaxed">
                 <VueMarkdown :source="truncateMarkdown(post.content, {limit: 250, ellipsis: true})" class="prose prose-sm max-w-none" />
             </p>
         </div>
